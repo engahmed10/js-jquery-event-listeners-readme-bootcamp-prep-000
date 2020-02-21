49c01bab71b01ca7eb4c79372200726b6efb71bf
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-21 05:23:49 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt()
 
 function pressIt()
 {
-  $("document").on("keydown", function(key) {
+  $("form").on("keydown", function(key) {
     
     if (key.which === "G") {
     alert('G is pressed');
